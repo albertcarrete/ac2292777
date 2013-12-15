c64d4dfaf474189cfda3feb74a705eddc3aad4b8
Albert Carrete <albertcarrete@gmail.com>
2013-12-15 01:37:40 -0800

    Minor changes

---
+++ Homework/Assignment4/Paycheck.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Paycheck Calculations</title>
+</head>
+
+<body>
+<?php
+    //Retrieve the form values
+	$payRate=$_GET["payRate"];
+	$hoursWorked=$_GET["hoursWorked"];
+	$dblTime=$_GET["dblTime"];
+	$tplTime=$_GET["tplTime"];
+
+	$timeHolder;
+
+	//Calculate the Paycheck
+	if($hoursWorked>$tplTime){
+		$payCheck = ( ($hoursWorked - $tplTime)*(3*$payRate) );
+		$payCheck += ( ($tplTime - $dblTime) * (2*$payRate) );
+		$payCheck += ($dblTime * $payRate);
+	}
+	elseif($hoursWorked >$dblTime){
+		$payCheck = ($dblTime - $hoursWorked) * (2*$payRate);
+		$payCheck += ($hoursWorked - $dblTime) * $payRate;
+		echo "Greater than double time!";
+	}
+	else{
+			echo "Regular!";
+
+		$payCheck = $hoursWorked * $payRate;
+	}
+
+
+
+	// $payCheck=$hoursWorked*$payRate;
+	// if($hoursWorked>$dblTime){
+	//    $payCheck+=($hoursWorked-$dblTime)*$payRate*2;
+	// }
+	// elseif($hoursWorked>$tplTime){
+	//    $payChck+=($hoursWorked-$dblTime)*$payRate*2;
+	// }
+
+	//Re-Display the inputs
+	echo "<p>Pay Rate = $$payRate/hr</p>";
+	echo "<p>Hours Worked = $hoursWorked(hrs)</p>";
+	echo "<p>Double time starts at $dblTime (hrs)</p>";
+	echo "<p>Triple time starts at $tplTime (hrs)</p>";
+	echo "<p>Pay Check = $$payCheck</p>";
+?>
+</body>
+</html>
--- readme.txt
+++ readme.txt
@@ -1,1 +1,10 @@
-Survey
+Admin Credentials for all 3 sites:
+==================================
+email: admin@gmail.com
+pass: admin
+
+
+
+Notes:
+==================================
+Some parts of the sites have issues with "Warning: Cannot modify header information" that only appears on the class server. Two ways to resolve this is to refresh the page or to run the sites locally. 
